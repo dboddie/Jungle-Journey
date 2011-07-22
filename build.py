278993b1ae107364c0a9317f6d2f137a2b338952
David Boddie <david@boddie.org.uk>
2011-07-23 01:32:29 +0200

    Updated the address of the code in the loader. Added the test program to the list of files to package.

--- build.py
+++ build.py
@@ -65,6 +65,9 @@ if __name__ == "__main__":
     t = read_basic("LOADER")
     files.append(("LOADER", 0xffff0e00, 0xffff802b, t))
     
+    t = read_basic("TESTVALUES")
+    files.append(("TEST", 0xffff0e00, 0xffff802b, t))
+    
     u = UEFfile.UEFfile(creator = 'build.py '+version)
     u.minor = 6
     u.import_files(0, files)
